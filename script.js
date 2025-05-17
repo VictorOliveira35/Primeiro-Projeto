1b53f94e12019c7dd21dea2ce111d6b5048df62d
unknown <victornsis@yahoo.com.br>
2025-05-17 11:34:13 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    //pegar a tag img
+    const img = document.querySelector('#profile img')
+
+    // substituir a imagem
+    if(html.classList.contains('light')){
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./assets/avatareu-light.jpg",)
+     }else {
+        //se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatarmeu.png")
+     }  
+     //alteração da descrição da imagem!
+     if(html.classList.contains('light')){
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("alt", "foto de victor oliveira de touca",)
+     }else {
+        //se tiver sem light mode, manter a imagem normal
+        img.setAttribute("alt", "foto de victor oliveira sorrindo")
+     }
+    
+}
